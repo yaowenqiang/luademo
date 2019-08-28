c98602b9f1a857e6af14878e4594536f805b32ae
yaowenqiang <yaowenqiang111@163.com>
2019-08-29 03:55:07 +0800

    add bool demo, 0 and empty string are true,

--- hello.lua
+++ hello.lua
@@ -9,3 +9,40 @@ local num
 print(num)
 num = 500
 print(num)
+
+local a = true
+
+local b = 0 
+-- 0 和nil 为 true
+
+local c = nil
+
+local d = ''
+
+if a then
+    print("a")
+else
+    print("not a")
+
+end
+
+if b then
+    print("b")
+else
+    print("not b")
+end
+
+if c then
+    print("c")
+else
+    print("not c")
+end
+
+
+
+if d then
+    print("d")
+else
+    print("not d")
+end
+
