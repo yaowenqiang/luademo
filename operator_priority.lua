277a5837eb0d88ef73846e4dbd4446438e4fbf78
yaowenqiang <yaowenqiang111@163.com>
2019-09-03 03:15:09 +0800

    add operator priority demo

---
+++ operator_priority.lua
@@ -0,0 +1,12 @@
+local a , b = 1,3
+local x ,y = 3, 4
+local i = 10
+local rest  = 0
+res = a + 1<b/2 + 1
+print(res)
+print("res is", res)
+print(res)
+res = 5 + x^2*8
+print(res)
+res = a < y and y <= x
+print(res)
