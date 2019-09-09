846f48c67ac7c3ab871d54b4df59cea9ada1d51d
yaowenqiang <yaowenqiang111@163.com>
2019-09-10 01:41:58 +0800

    add more string function demo, include string.len, string.upper,     string.lower and so on

--- strings.lua
+++ strings.lua
@@ -1,5 +1,6 @@
 print("Hello " .. "Wrodl")
 
+
 print(0 .. 1)
 
 str1 = string.format("%s-%s", "hello" ,"world")
@@ -20,5 +21,20 @@ for i, elem in ipairs(my_list) do
 end
 
 local res = table.concat(places)
-
 print(res)
+
+print(string.byte("abc", 1,2))
+print(string.byte("ABC", 1))
+print(string.byte("ABC"))
+
+
+print(string.char(96,97,98))
+print(string.char())
+print(string.char(65,66))
+
+print(string.upper('Hello lua'))
+
+print(string.lower("Hello lua"))
+
+print(string.len("Hello lua"))
+print(string.len("你"))   -- 一个汉字3字节
