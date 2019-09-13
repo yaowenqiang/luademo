6d4b1ebe8f5d22050f7f22abd8310d93608af044
yaowenqiang <yaowenqiang111@163.com>
2019-09-13 22:18:59 +0800

    add table demo

---
+++ tables.lua
@@ -0,0 +1,10 @@
+local color = {first="red", "blue", third="green", "yellow"}
+
+print(color["first"])
+print(color[1])
+print(color[0])
+print(color[-1])
+print(color["third"])
+print(color[2])
+print(color[3])
+print(color[33])
