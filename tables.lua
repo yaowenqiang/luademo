afcc56d70a3b86baf546d77071e5a70d123ab2ba
yaowenqiang <yaowenqiang111@163.com>
2019-09-14 12:13:40 +0800

    add get table length demo

--- tables.lua
+++ tables.lua
@@ -8,3 +8,21 @@ print(color["third"])
 print(color[2])
 print(color[3])
 print(color[33])
+
+
+local tbTest1 = {1, a=2, 1}
+--print("Test1 length:" .. table.getn(tbTest1))
+print("Test1 length:" .. (#tbTest1))
+
+local table2 = {1,nil}
+print("Test2 " .. table.getn(table2))
+print("Test2 " .. (#table2))
+
+local table3 = {1,nil,2}
+print("Test3 " .. table.getn(table3))
+print("Test3 " .. (#table3))
+
+local table4 = {1,nil,2, 3, nil, 4}
+print("Test4 " .. table.getn(table4))
+print("Test4 " .. (#table4))
+
