0f7d3ed2e2eaf90571e8616496870926e25b4a4d
yaowenqiang <yaowenqiang111@163.com>
2019-09-15 20:25:38 +0800

    add more table function demo

--- tables.lua
+++ tables.lua
@@ -1,3 +1,5 @@
+--luarocks install penlight
+require "pl"
 local color = {first="red", "blue", third="green", "yellow"}
 
 print(color["first"])
@@ -26,3 +28,67 @@ local table4 = {1,nil,2, 3, nil, 4}
 print("Test4 " .. table.getn(table4))
 print("Test4 " .. (#table4))
 
+
+
+local tb1 = {}
+print({#tb1})
+print(table.getn{tb1})
+print({tb1[1]})
+
+local tb2 = {0}
+print({tb2[1]})
+
+
+local a = {1,3,4,"hello"}
+
+--table.concat
+
+print(table.concat(a))
+print(table.concat(a,"|"))
+print(table.concat(a,'|'))
+print(table.concat(a,' ', 2,4))
+print(table.concat(a,' ', 4,2))
+
+--table.insert
+
+
+local b = {1,0}
+table.insert(b, 1,3)
+print(b[1],b[2],b[3])
+table.insert(b, 10)
+print(b[1],b[2],b[3], b[4])
+
+
+--table.maxn
+
+local a = {}
+a[-1] = 10
+print(table.maxn(a))
+a[5] = 10
+print(table.maxn(a))
+a[6] = nil
+print(table.maxn(a))
+
+--table.remove
+
+local a = {1,2,3,4}
+print(table.remove(a,1))
+print(a[1],a[2],a[3], a[4])
+print(table.remove(a))
+print(a[1],a[2],a[3], a[4])
+
+--table.sort
+
+local function compare(x,y)
+    return x > y    
+end
+
+local a = {1,7,3,4,5,6,5,4,3}
+print("beforea sort ;")
+pretty.dump(a)
+print("after sort ;")
+table.sort(a)
+pretty.dump(a)
+
+table.sort(a, compare)
+pretty.dump(a)
