ed180697a22ccb5ad6053bb715d73d759f891733
yaowenqiang <yaowenqiang111@163.com>
2019-09-29 01:47:15 +0800

    move api code into lua file

---
+++ lua/common/param.lua
@@ -0,0 +1,20 @@
+local _M = {}
+function _M.is_number (...)
+    local args = {...}
+
+    local num
+
+    for _, v in ipairs(args) do
+        num = tonumber(v)
+        if nil == num then
+            return false 
+        end
+    end
+    return true     
+end
+
+return _M
+
+
+
+
