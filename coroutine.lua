abcd15ba4b80e02fa4922e8d590c5fef13e9a6c2
Yaowenqiang <yaowenqiang111@163.com>
2019-10-29 21:32:21 +0800

    add coroutine yield demo

--- coroutine.lua
+++ coroutine.lua
@@ -3,6 +3,51 @@ co = coroutine.create(function()
     end
 )
 print(co)
+
+co1 = coroutine.create(function()
+    for i = 1, 10 do
+        print("co ", i)
+        coroutine.yield()
+    end
+
+end
+)
 print(coroutine.status(co))
 coroutine.resume(co)
 print(coroutine.status(co))
+
+coroutine.resume(co1)
+print(coroutine.status(co1))
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+coroutine.resume(co1)
+
+
+co3 = coroutine.create(function(a,b,c)
+    print("co ", a,b,c) 
+end)
+
+
+coroutine.resume(co3, 1,2,3)
+
+
+
+co4 = coroutine.create(function(a,b)
+    coroutine.yield(a + b ,a - b)
+end)
+
+print(coroutine.resume(co4, 20, 10))
