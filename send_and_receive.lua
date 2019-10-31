2b860c5b2043b3afa83ffcca9a80324519e5b526
Yaowenqiang <yaowenqiang111@163.com>
2019-10-31 23:05:44 +0800

    add producer and receiver demo

--- send_and_receive.lua
+++ send_and_receive.lua
@@ -6,7 +6,15 @@ function producer ()
 
 end
 
+function receive()
+    local status, value = coroutine.resume(producer)
+    print("receive: ", value)
+    return value
+end
 
+function send(x)
+    coroutine.yield(x)
+end
 
 function consumer ()
     while true do
@@ -15,3 +23,16 @@ function consumer ()
     end
 end
 
+
+producer = coroutine.create(function() 
+    while true do
+        local x = io.read()
+        print("produced : ", x)
+        send(x)
+    end
+end)
+
+
+while true do
+    receive()
+end
