c68e53f59e637e2eb29d9342ce065dbd30311bcb
Yaowenqiang <yaowenqiang111@163.com>
2019-11-01 21:17:17 +0800

    add filter as consumer and producer demo

--- send_and_receive.lua
+++ send_and_receive.lua
@@ -6,9 +6,9 @@ function producer ()
 
 end
 
-function receive()
-    local status, value = coroutine.resume(producer)
-    print("receive: ", value)
+function receive(prod)
+    local status, value = coroutine.resume(prod)
+    --print("receive: ", value)
     return value
 end
 
@@ -16,23 +16,43 @@ function send(x)
     coroutine.yield(x)
 end
 
-function consumer ()
+function consumer (prod)
     while true do
-        local x = io.receive()
+        local x = receive(prod)
         io.write(x, "\n")
     end
 end
 
 
-producer = coroutine.create(function() 
-    while true do
-        local x = io.read()
-        print("produced : ", x)
-        send(x)
-    end
-end)
-
-
-while true do
-    receive()
+function producer ()
+    return coroutine.create(function() 
+        while true do
+            local x = io.read()
+            --print("produced : ", x)
+            send(x)
+        end
+    end)
 end
+
+function filter(prod)
+    return coroutine.create(function() 
+        local line = 1
+        while true do
+            local x = receive(prod)
+            x = string.format("%5d %s", line, x)
+            send(x)
+            line = line + 1
+        end
+    end)
+
+end
+
+
+--while true do
+    --receive()
+    p = producer()
+    f = filter(p)
+    consumer(f)
+    --consumer(filter(produce()
+
+--end
