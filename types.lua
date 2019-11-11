b5c2852f7dbcd3414c5a8ad292993c9e35774d18
Yaowenqiang <yaowenqiang111@163.com>
2019-11-11 22:35:57 +0800

    add strings demo

---
+++ types.lua
@@ -0,0 +1,19 @@
+print("[")
+print("\[\"abc\"\]")
+print("\[[\"abc\"\]]")
+print([[
+[\[\"abc\"/]/]
+]])
+--[[
+    heoo
+
+
+]]--
+
+content = [[
+    "abc"
+    'abc'
+"abc'"
+"abc"dfd"dfd''df'
+]]
+io.write(content)
