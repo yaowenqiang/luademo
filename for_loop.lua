04de3d0b40a879bdd45818e5ff1da931d8cba15b
Yaowenqiang <yaowenqiang111@163.com>
2019-11-20 22:36:16 +0800

    add ipairs for llop demo

--- for_loop.lua
+++ for_loop.lua
@@ -11,3 +11,23 @@ end
 for i = 10, 1, -1 do
     print("for " .. i)
 end
+
+local days = {
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+}
+
+local newDays = {};
+for i, v in ipairs(days) do
+  newDays[i] = v
+end
+
+for i, v in pairs(newDays) do
+    print(newDays[i])
+end
+
