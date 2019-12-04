f8628e65e59024aa166873a3bb3b6eae81111b79
Yaowenqiang <yaowenqiang111@163.com>
2019-12-04 22:30:57 +0800

    add table sort with function demo

--- functions.lua
+++ functions.lua
@@ -440,3 +440,22 @@ function foo (x) return 2 * x end
 a.p(foo(2))
 foo = function(x) return 2 * x end
 a.p(foo(3))
+
+
+network = {
+	{name = "grauna", IP = "210.26.30.34"},
+	{name = "arraial",  IP = "210.26.30.23"},
+	{name = "lua",  IP = "210.26.23.12"},
+	{name = "derain", IP = "210.26.23.20"},
+}
+
+table.sort(network, function (a,b)
+		return a.name > b.name
+end)
+
+
+
+for k, v in  ipairs(network) do
+		print(k, v)	
+end
+
