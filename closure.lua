6776c07a1edfd12a63707e78fa90f04fb225174c
Yaowenqiang <yaowenqiang111@163.com>
2019-12-06 22:35:32 +0800

    add more  closure demo

--- closure.lua
+++ closure.lua
@@ -18,5 +18,21 @@ function sortByGrade(names, grades)
 end
 
 
-sortByGrade(names, grade)
+sortByGrade(names, grades)
 
+
+function new_counter()
+  local i = 0
+  return function ()
+      i = i + 1
+      return i
+  end
+
+end
+
+c1 = new_counter();
+print(c1())
+print(c1())
+print(c1())
+print(c1())
+print(c1())
