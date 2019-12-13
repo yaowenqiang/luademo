c5f2a27c16f34bd038fee926cac6405ec09325dc
Yaowenqiang <yaowenqiang111@163.com>
2019-12-13 22:44:56 +0800

    add allwords function demo

--- literator.lua
+++ literator.lua
@@ -26,3 +26,26 @@ for element in list_iter(t) do
 end
 
 
+function allwords()
+    local line = io.read()
+    local pos = 1
+    return function ()
+        while line do
+            local s,e = string.find(line, "%w+", pos)
+            if s then
+              pos = e + 1
+              return string.sub(line, s, e)
+            else
+              line = io.read()
+              pos = 1
+            end
+        end
+        return nil
+    end
+
+end
+
+
+for word in allwords() do
+  print(word)
+end
