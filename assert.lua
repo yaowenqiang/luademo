6c4b24a368ea56c6d8a47f059f9931606cb426b8
Yaowenqiang <yaowenqiang111@163.com>
2019-12-21 22:43:22 +0800

    add assert and error demo

---
+++ assert.lua
@@ -0,0 +1,14 @@
+print("enter a number")
+n = io.read("*number")
+if not n then error("invalide input") end
+
+print("enter a number")
+m = assert(io.read("*number"), "invalid input")
+
+
+if not tonumber('abc') then 
+  error("wrong ")
+end
+assert(math.sin("abc"))
+
+
