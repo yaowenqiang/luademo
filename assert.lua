13315a2ab5db79d5d63dd8a81b980e7ef0b7b8ad
Yaowenqiang <yaowenqiang111@163.com>
2019-12-22 21:34:28 +0800

    add io read and open error handling demo

--- assert.lua
+++ assert.lua
@@ -6,9 +6,24 @@ print("enter a number")
 m = assert(io.read("*number"), "invalid input")
 
 
-if not tonumber('abc') then 
-  error("wrong ")
-end
-assert(math.sin("abc"))
+--if not tonumber('abc') then 
+--  error("wrong ")
+--end
+--assert(math.sin("abc"))
 
 
+local file, msg
+
+repeat
+
+  print "enter a file name:"
+  local name = io.read()
+
+  if not name then return end
+  file, msg = io.open(name, "r")
+
+  if not file then print(msg) end
+until file
+
+
+file = assert(io.open(io.read(), "r"))
