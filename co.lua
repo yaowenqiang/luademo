01c971bba8116407873d402556fa1c5606874955
Yaowenqiang <yaowenqiang111@163.com>
2019-12-27 22:10:37 +0800

    add more coroutine demo

--- co.lua
+++ co.lua
@@ -39,3 +39,18 @@ coroutine.resume(co2)
 print(coroutine.status(co2))
 coroutine.resume(co2)
 print(coroutine.status(co2))
+
+co3 = coroutine.create(function(a,b , c)
+    print("co", a,b,c) 
+end)
+
+coroutine.resume(co3,1,2,3)
+
+
+co4 = coroutine.create(function(a, b)
+    coroutine.yield(a + b, a - b) 
+end)
+
+print(coroutine.resume(co4,20, 10))
+print(coroutine.resume(co4,20, 10))
+print(coroutine.resume(co4,20, 10))
