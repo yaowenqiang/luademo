08e1db2af50860aa4a8994acffe8f044c0c015b4
Yaowenqiang <yaowenqiang111@163.com>
2019-12-29 22:59:33 +0800

    add producer/consumer demo

--- co.lua
+++ co.lua
@@ -78,3 +78,38 @@ print(coroutine.resume(co5))
 print(coroutine.resume(co5))
 print(coroutine.resume(co5))
 print(coroutine.resume(co5))
+
+
+function producer ()
+    while true do
+        local x = io.read()
+        send(x)
+    end
+end
+
+function consumer()
+    while true do
+        local x = receive()
+        io.write(x, "\n")
+    end
+end
+
+function receive()
+    local status, value = coroutine.resume(producer)
+    return value
+end
+
+function send(x)
+    coroutine.yield(x)
+end
+
+
+producer = coroutine.create(function() 
+  while true do
+      local x = io.read()
+      send(x)
+  end
+end)
+
+
+consumer()
