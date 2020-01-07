e857ebbe0e05a3d898fbd3900fb09c09074c12ec
Yaowenqiang <yaowenqiang111@163.com>
2020-01-07 22:08:57 +0800

    add serialize table demo

--- data_structure/serialize.lua
+++ data_structure/serialize.lua
@@ -6,7 +6,17 @@ if type(o) == "number" then
     --io.write("'" , o, "'")
     --io.write("[[", o , "]]")
     io.write(string.format("%q", o))
+  elseif type(o) == "table" then
+    io.write("{\n")
+    for k, v in pairs(o) do
+        io.write(" ", k, " = ")
+        serialize(v)
+        io.write(",\n")
+    end
+    io.write("}\n")
+    else
+      io.write("can not serialize a  " , type(o))
 end
 
 end
-serialize("serialize")
+serialize({a = 1, b = 2, c = {d = 5}})
